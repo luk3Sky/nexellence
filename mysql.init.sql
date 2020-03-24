fda542bfca889e6e2f6057741b775fff719dec8b
luk3Sky <ganindudananja@gmail.com>
2020-03-24 17:32:10 +0530

    Teacher table creation

--- mysql.init.sql
+++ mysql.init.sql
@@ -20,3 +20,19 @@ CREATE TABLE IF NOT EXISTS student (
     last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS teacher (
+    id VARCHAR(32),
+	first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    address VARCHAR(255),
+    city VARCHAR(255),
+    province VARCHAR(255),
+    zip VARCHAR(8),
+    title VARCHAR(255),
+    phone VARCHAR(20),
+    mobile_phone VARCHAR(20),
+    email VARCHAR(200),
+    pic VARCHAR(255),
+    PRIMARY KEY (id)
+);
