a8e09b63ddfb5179dcbc9218a74a924a2624324f
luk3Sky <ganindudananja@gmail.com>
2020-03-24 17:27:10 +0530

    Student table creation

---
+++ mysql.init.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS Nexcellence;
+USE Nexcellence;
+-- select length(md5(uuid()));
+
+-- Table Creation
+CREATE TABLE IF NOT EXISTS student (
+    id VARCHAR(32),
+	first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    address VARCHAR(255),
+    city VARCHAR(255),
+    province VARCHAR(255),
+    zip VARCHAR(8),
+    dob DATE,
+    phone VARCHAR(20),
+    mobile_phone VARCHAR(20),
+    email VARCHAR(200),
+    pic VARCHAR(255),
+    registration TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id)
+);
