e24016aafd4a89d9a28fff582f9a423a9e962279
luk3Sky <ganindudananja@gmail.com>
2020-04-22 20:18:54 +0530

    Server settings

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
 var express = require('express'),
     bodyParser = require('body-parser'),
     compression = require('compression'),
