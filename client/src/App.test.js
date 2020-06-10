72e6b05c36575e1c15c36b52b2a05307f1133f1d
luk3Sky <ganindudananja@gmail.com>
2020-06-11 04:37:35 +0530

    Jest test setup added for CI

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {cleanup, fireEvent, render} from '@testing-library/react';
 import App from './App';
 
-// Note: running cleanup afterEach is done automatically for you in @testing-library/react@9.0.0 or higher
-// unmount and cleanup DOM after the test is finished.
-afterEach(cleanup);
-
-it('CheckboxWithLabel changes the text after click', () => {
-  const {queryByLabelText, getByLabelText} = render(
-    <App labelOn="On" labelOff="Off" />,
-  );
-
-  expect(queryByLabelText(/off/i)).toBeTruthy();
-
-  fireEvent.click(getByLabelText(/off/i));
-
-  expect(queryByLabelText(/on/i)).toBeTruthy();
-});
-
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
